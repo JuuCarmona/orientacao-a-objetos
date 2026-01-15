124ef86aacc39f2dea08267bc04ff5dabbe7885e
Julia Carmona <168484665+JuuCarmona@users.noreply.github.com>
2026-01-15 18:26:26 -0300

    main.py

---
+++ importacao_reutilizacao/livros/main.py
@@ -0,0 +1,7 @@
+from oo import Livro
+
+livro_main1 = Livro("python para iniciante",'Carlos Coder',2021)
+livro_main2 = Livro('Web Development Essentials','Laura Developer',2023)
+
+print(livro_main1)
+print(livro_main2)
