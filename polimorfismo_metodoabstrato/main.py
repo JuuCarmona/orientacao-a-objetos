33be37bd6a13df19d4110711b3b2e6dd3eb91b59
Julia Carmona <168484665+JuuCarmona@users.noreply.github.com>
2026-02-27 03:09:00 -0300

    Add main.py to demonstrate Carro class usage

---
+++ polimorfismo_metodoabstrato/main.py
@@ -0,0 +1,7 @@
+from carro import Carro
+
+carro1 = Carro(marca = 'ford',modelo = 'focus',cor = 'preto')
+carro2 = Carro(marca = 'chevrolet',modelo = 'cruze',cor = 'prata')
+ 
+print(f'Carro 1:{carro1.marca},{carro1.modelo},{carro1.cor}')
+print(f'Carro 2:{carro2.marca},{carro2.modelo},{carro2.cor}')
