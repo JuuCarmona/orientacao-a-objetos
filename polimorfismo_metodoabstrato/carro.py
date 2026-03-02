1d5600c651187c08d825a51c7d644e68cb87be00
Julia Carmona <168484665+JuuCarmona@users.noreply.github.com>
2026-03-02 15:41:35 -0300

    Delete polimorfismo_metodoabstrato/carro.py

--- polimorfismo_metodoabstrato/carro.py
+++
@@ -1,9 +0,0 @@
-from veiculo import Veiculo
-
-class Carro(Veiculo):
-    def __init__(self,marca,modelo,cor):
-        super().__init__(marca,modelo)
-        self.cor = cor
-    
-    def ligar(self):
-        print(f'o carro {self.modelo} está ligado')
