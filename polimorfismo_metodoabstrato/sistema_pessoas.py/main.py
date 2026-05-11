20ba91cdfd51547343c538c78b656d714e60a917
Julia Carmona <168484665+JuuCarmona@users.noreply.github.com>
2026-05-11 19:44:52 -0300

    Add main script to instantiate Aluno and Professor

---
+++ polimorfismo_metodoabstrato/sistema_pessoas.py/main.py
@@ -0,0 +1,9 @@
+from aluno import Aluno
+from professor import Professor
+
+
+pessoa1 = Aluno (nome = "Julia",idade = 21, curso = "ciência da computação")
+pessoa2 = Professor(nome = "Gustavo",idade = 48,disciplina = "POO")
+
+pessoa1.apresentar()
+pessoa2.apresentar()
