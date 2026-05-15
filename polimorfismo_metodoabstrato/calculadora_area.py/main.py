fcbe292a77a9f45c8d4072b826de91b9ab4f9935
Julia Carmona <168484665+JuuCarmona@users.noreply.github.com>
2026-05-15 07:29:57 -0300

    Add main script to calculate areas of shapes

---
+++ polimorfismo_metodoabstrato/calculadora_area.py/main.py
@@ -0,0 +1,8 @@
+from quadrado import Quadrado
+from circulo import Circulo
+
+forma1  = Quadrado(cor = "vermelho",base = 10, altura = 2)
+forma2 = Circulo(cor = "verde",raio = 5)
+
+forma1.calcular_area()
+forma2.calcular_area()
